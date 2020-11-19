3d1926f8f32d44f464cdecbdd695c1c1d92343ef
surgoel93 <69698303+surgoel93@users.noreply.github.com>
2020-11-19 15:41:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# c20-hetansh
-bounceoff algo
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(1200,800);
+
+  fixedRect = createSprite(200,200,50,80);
+  movingRect = createSprite(400,200,80,30);
+
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+
+  movingRect.debug = true;
+fixedRect.debug = true;
+
+}
+
+function draw() {
+  background("pink");  
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 &&
+    movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 &&
+    fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+
+  }
+  else {
+
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
